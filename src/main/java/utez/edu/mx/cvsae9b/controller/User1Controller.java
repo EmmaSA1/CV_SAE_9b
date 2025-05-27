4d9719809e1dd23ca6012a607c15245e22bd107e
EmmaSA1 <20223tn079@utez.edu.mx>
2025-05-27 16:22:52 -0600

    [ADD] se agrego el controlador User1Controller.java

---
+++ src/main/java/utez/edu/mx/cvsae9b/controller/User1Controller.java
@@ -0,0 +1,18 @@
+package utez.edu.mx.cvsae9b.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/user1")
+@CrossOrigin("*")
+public class User1Controller {
+
+    @GetMapping("")
+    public String getMessage() {
+        return "Hola desde el controlador 1";
+    }
+}
