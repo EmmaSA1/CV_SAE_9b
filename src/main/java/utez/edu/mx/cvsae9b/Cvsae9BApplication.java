5e47f34a57e866fc0035e883a404e5aeec3ad17f
EmmaSA1 <20223tn079@utez.edu.mx>
2025-05-27 15:50:41 -0600

    [FIRST COMMIT] subida inicial de proyecto

---
+++ src/main/java/utez/edu/mx/cvsae9b/Cvsae9BApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvsae9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvsae9BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvsae9BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cvsae9b/Cvsae9BApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cvsae9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvsae9BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
